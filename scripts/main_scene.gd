63437b06cebd4d5f0b3c14be968715974b084690
kbake <kbake@outlook.com>
2026-04-10 05:01:41 -0400

    Simple project setup

---
+++ scripts/main_scene.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var stage_instance : Node2D
+
+func _on_button_pressed():
+	load_stage("stage")
+
+func unload_stage():
+	if (is_instance_valid(stage_instance)):
+		stage_instance.queue_free()
+	stage_instance = null
+
+func load_stage(stage_name : String):
+	unload_stage()
+	var stage_path := "res://scenes/%s.tscn" % stage_name
+	var stage_resource := load(stage_path)
+	if( stage_resource ):
+		stage_instance = stage_resource.instantiate()
+		self.add_child((stage_instance))
---
+++ scripts/player.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+# has whatever a player needs to keep track of
