997ebea24688e9cc1f8066a2c4918e6520ed66f4
DigitalValiance <silverfrost12@gmail.com>
2026-04-11 16:00:51 -0400

    Exposed basic features to the UI on the player for quick iteration.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,3 +1,8 @@
 extends Node2D
 
 # has whatever a player needs to keep track of
+@export var Player_Health : int = 100
+@export var Player_Speed : int = 50
+@export var is_Blocking : bool = false
+@export var is_Shoving : bool = false
+@export var is_Grabbing : bool = false
